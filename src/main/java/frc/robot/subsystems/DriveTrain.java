9e1fc0a79cfb5bfa7cbf6db063576220e2a3e84b
Julian Allchin <julian@allchin.com>
2019-11-18 15:45:17 -0800

    Refractor

---
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -0,0 +1,50 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.commands.DriveWithJoysticks;
+
+public class DriveTrain extends Subsystem {	
+
+	// Left motor group
+	Talon m_frontLeft = null;
+	Talon m_rearLeft = null;
+	SpeedControllerGroup m_left = null;
+
+	// Right motor group
+	Talon m_frontRight = null;
+	Talon m_rearRight = null;
+	SpeedControllerGroup m_right = null;
+
+	// Initialize differential drive
+	public DifferentialDrive differentialDrive = null;
+
+
+	public DriveTrain(){
+
+		// Left motor group
+		m_frontLeft = new Talon(RobotMap.m_frontLeft);
+		m_rearLeft = new Talon(RobotMap.m_rearLeft);
+		m_left = new SpeedControllerGroup(m_frontLeft, m_rearLeft);
+
+		// Right motor group
+		m_frontRight = new Talon(RobotMap.m_frontRight);
+		m_rearRight = new Talon(RobotMap.m_rearRight);
+		m_right = new SpeedControllerGroup(m_frontRight, m_rearRight);
+
+		// Initialize differential drive
+		differentialDrive = new DifferentialDrive(m_left, m_right);
+	}
+
+	public void Drive() {
+		differentialDrive.tankDrive(Robot.m_oi.getLeftSpeed(), Robot.m_oi.getRightSpeed());
+	}
+
+	@Override
+	public void initDefaultCommand() {
+		setDefaultCommand(new DriveWithJoysticks());
+	}
+}
